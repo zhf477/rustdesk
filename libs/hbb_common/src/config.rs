c7d3abbd63923b608ba2999c723f55192d0bf1e2
zhf477 <46109205+zhf477@users.noreply.github.com>
2024-11-03 02:40:33 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["zhf.hxuan.fun"];
+pub const PUBLIC_RS_PUB_KEY: &str = "V1GBPO6i4hWMUNjEsobPcQuyz3d0lc0qLoA1iXmYpNM=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
